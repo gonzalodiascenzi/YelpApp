a35b9b28493a5cbc51f850ff30a860f0130508f1
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2022-02-24 11:20:20 -0300

    Creamos rama dev

--- app/src/main/java/com/example/yelpapp/ui/NavHostActivity.kt
+++ app/src/main/java/com/example/yelpapp/ui/NavHostActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.yelpapp.R
 
-
 class NavHostActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
