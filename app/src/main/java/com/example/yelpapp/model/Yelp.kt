a60a195011fd5f9133d468a7e5a269dbad0d7953
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2022-02-28 11:02:05 -0300

    Proyecto base

---
+++ app/src/main/java/com/example/yelpapp/model/Yelp.kt
@@ -0,0 +1,7 @@
+package com.example.yelpapp.model
+
+data class Yelp(
+    val name: String,
+    val location: String
+)
+
--- app/src/main/java/com/example/yelpapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/yelpapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.yelpapp
+package com.example.yelpapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.yelpapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
