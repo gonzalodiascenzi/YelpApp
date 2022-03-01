08db2a062f59c14355760cc61f87b4ab42cb3c78
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2022-03-01 08:31:03 -0300

    Implementacion de Librerias, Gradles y Android Manifest (close #10)

--- app/src/main/java/com/example/yelpapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/yelpapp/ui/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.yelpapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.yelpapp.R
+import com.example.yelpapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
