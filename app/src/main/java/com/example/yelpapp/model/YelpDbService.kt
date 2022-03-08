6607a4e2e3c8f05fefec24fbefce83ad9c6f5229
Arturo Wilson <info@jawil.com.ar>
2022-03-08 13:01:08 -0300

    Commit with Class Data Class and Service

--- app/src/main/java/com/example/yelpapp/model/YelpDbService.kt
+++ app/src/main/java/com/example/yelpapp/model/YelpDbService.kt
@@ -1,4 +1,24 @@
 package com.example.yelpapp.model
 
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
 interface YelpDbService {
-}
+
+
+
+        @GET("/v3/businesses/search")
+        fun getYelpSearch(
+            @Header("Authorization") api: String,
+            @Query("term") term: String,
+            @Query("latitude") latitude: Double,
+            @Query("longitude") longitude: Double
+        ): Call<Yelp>
+    }
+
+
+
+
