0b5f4a71a3fa627b4e03c9090511b1079c6e3531
Arturo Wilson <info@jawil.com.ar>
2022-03-12 18:19:39 -0300

    Create Nav Host Activity ,mainFragment and DetailFragment were created the corresponding libraries and that of SafeArg were included

---
+++ app/src/main/java/com/example/yelpapp/ui/DetailFragment.kt
@@ -0,0 +1,18 @@
+package com.example.yelpapp.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.yelpapp.R
+import com.example.yelpapp.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentDetailBinding.bind(view)
+
+
+    }
+}
