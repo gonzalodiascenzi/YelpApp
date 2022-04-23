7d733b63559b714761fe477a2268d9eb23b48f57
simonipd <2012inca1E18>
2022-04-23 16:46:43 -0400

    Impementation GetBusinessUseCase.kt (domain) complete & libs (dependencies)

--- usecases/src/main/java/com/example/yelpapp/usecases/GetBusinessUseCase.kt
+++ usecases/src/main/java/com/example/yelpapp/usecases/GetBusinessUseCase.kt
@@ -1,5 +1,10 @@
 package com.example.yelpapp.usecases
 
-class GetBusinessUseCase {
-    operator fun invoke() = ""
+import com.example.yelpapp.data.repository.BusinessRepository
+import javax.inject.Inject
+
+class GetBusinessUseCase @Inject constructor(
+    private val businessRepository: BusinessRepository
+){
+    operator fun invoke() = businessRepository.business
 }
