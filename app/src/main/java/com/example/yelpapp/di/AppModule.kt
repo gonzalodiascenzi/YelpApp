9f80ec8d8337cae4b23ded0fd6b1398049787ffd
simonipd <2012inca1E18>
2022-04-25 02:49:58 -0400

    Impementation AppModule

--- app/src/main/java/com/example/yelpapp/di/AppModule.kt
+++ app/src/main/java/com/example/yelpapp/di/AppModule.kt
@@ -1,11 +1,30 @@
 package com.example.yelpapp.di
 
+import android.app.Application
+import androidx.room.Room
+import com.example.yelpapp.data.database.BusinessDataBase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application) =
+        Room.databaseBuilder(
+            app,
+            BusinessDataBase::class.java,
+            "business-db"
+        ).build()
+
+    @Provides
+    @Singleton
+    fun provideBusinessDao(db: BusinessDataBase) =
+        db.businessDao()
 
 }
